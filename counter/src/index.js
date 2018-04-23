0379810efecb9922c6d3c76b596bfd3a8fd90dab
Francis Rodrigues <francis.rod01@outlook.com>
2018-04-23 20:37:32 -0300

    [counter] Incorporating the router

--- counter/src/index.js
+++ counter/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { render } from 'react-dom'
+import { Router, Route, hashHistory } from 'react-router'
 
 import { App } from './components/App'
+import { Whoops404 } from './components/Whoops404'
 
 window.React = React
 
 render(
-  <App />,
+  <Router history={hashHistory}>
+    <Route path="/" component={App} />
+    <Route path="*" component={Whoops404} />
+  </Router>,
   document.getElementById('react-container')
 )
