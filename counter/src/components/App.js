d83d5d248c88fe26c9daa6fb3dd12a4ac6a6684a
Francis Rodrigues <francis.rod01@outlook.com>
2018-04-23 11:14:42 -0300

    [counter] Working with states

---
+++ counter/src/components/App.js
@@ -0,0 +1,36 @@
+import createClass from 'create-react-class'
+
+
+export const App = createClass({
+  getInitialState() {
+    return {
+      allSkiDays: [
+        {
+          resort: "Squaw Valley",
+          date: new Date("1/2/2016"),
+          powder: true,
+          backcountry: false
+        },
+        {
+          resort: "Kirkwood",
+          date: new Date("3/2/2016"),
+          powder: false,
+          backcountry: false
+        },
+        {
+          resort: "Mt. Tallac",
+          date: new Date("4/2/2016"),
+          powder: false,
+          backcountry: true
+        }
+      ]
+    }
+  },
+  render() {
+    return (
+      <div className="app">
+        {this.state.allSkiDays[0]["resort"]}
+      </div>
+    )
+  }
+})
--- counter/src/index.js
+++ counter/src/index.js
@@ -2,35 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { render } from 'react-dom'
 
-import { SkiDayList } from './components/SkiDayList'
+import { App } from './components/App'
 
 window.React = React
 
-
-const daysList = [
-  {
-    resort: "Squaw Valley",
-    date: new Date("1/2/2016"),
-    powder: true,
-    backcountry: false
-  },
-  {
-    resort: "Kirkwood",
-    date: new Date("3/2/2016"),
-    powder: false,
-    backcountry: false
-  },
-  {
-    resort: "Mt. Tallac",
-    date: new Date("4/2/2016"),
-    powder: false,
-    backcountry: true
-  }
-]
-
 render(
-  <SkiDayList
-    days={daysList}
-  />,
+  <App />,
   document.getElementById('react-container')
 )
