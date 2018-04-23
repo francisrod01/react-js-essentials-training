1dbcb24c8a6170e8d03044453b113eb33b623d98
Francis Rodrigues <francis.rod01@outlook.com>
2018-04-23 11:04:28 -0300

    [counter] Custom validation using PropTypes

--- counter/src/components/SkiDayList.js
+++ counter/src/components/SkiDayList.js
@@ -29,5 +29,13 @@ export const SkiDayList = ({ days }) => (
 )
 
 SkiDayList.propTypes = {
-  days: PropTypes.array
+  days: function(props) {
+    if (!Array.isArray(props.days)) {
+      return new Error("SkiDayList should be an array")
+    } else if (!props.days.length) {
+      return new Error("SkiDayList must have at least one record")
+    } else {
+      return null
+    }
+  }
 }
