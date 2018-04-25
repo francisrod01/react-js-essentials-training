61f64feb3a1e394cb10eca9402b3cb1c5171b478
Francis Rodrigues <francis.rod01@outlook.com>
2018-04-25 07:43:26 -0300

    [react-router] Building the MemberList component

--- react-router-demo/src/index.js
+++ react-router-demo/src/index.js
@@ -6,18 +6,8 @@ import { Router, Route, hashHistory } from 'react-router'
 import './app.css'
 
 // import routes from './routes'
-import Member from './components/ui/Member'
+import MemberList from './components/ui/MemberList'
 
 window.React = React
 
-
-const member = (
-  <Member admin={true}
-    name="Edna Welch"
-    email="edna.welch88@example.com"
-    thumbnail="https://randomuser.me/api/portraits/women/90.jpg"
-    makeAdmin={(email) => console.log(email)}
-  />
-)
-
-render(member, document.getElementById('react-container'))
+render(<MemberList />, document.getElementById('react-container'))
