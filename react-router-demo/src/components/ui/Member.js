662957406d49ba76106a7bf8349ad56781d7049b
Francis Rodrigues <francis.rod01@outlook.com>
2018-04-25 07:31:26 -0300

    [react-router] Create a Member component

---
+++ react-router-demo/src/components/ui/Member.js
@@ -0,0 +1,16 @@
+import { Component } from 'react'
+import FaShield from 'react-icons/lib/fa/shield'
+
+class Member extends Component {
+  render() {
+    return (
+      <div className="member">
+        //
+        // TODO: Create Member Component
+        //
+      </div>
+    )
+  }
+}
+
+export default Member
--- react-router-demo/src/index.js
+++ react-router-demo/src/index.js
@@ -5,8 +5,19 @@ import { Router, Route, hashHistory } from 'react-router'
 
 import './app.css'
 
-import routes from './routes'
+// import routes from './routes'
+import Member from './components/ui/Member'
 
 window.React = React
 
-render(routes, document.getElementById('react-container'))
+
+const member = (
+  <Member admin={true}
+    name="Edna Welch"
+    email="edna.welch88@example.com"
+    thumbnail="https://randomuser.me/api/portraits/women/90.jpg"
+    makeAdmin={(email) => console.log(email)}
+  />
+)
+
+render(member, document.getElementById('react-container'))
