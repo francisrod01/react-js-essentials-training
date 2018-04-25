a686596e4872ba577c56b42af3386d63fd72642c
Francis Rodrigues <francis.rod01@outlook.com>
2018-04-25 08:00:16 -0300

    [react-router] Understanding the mounting lifecycle

--- react-router-demo/src/components/ui/Member.js
+++ react-router-demo/src/components/ui/Member.js
@@ -2,11 +2,16 @@ import { Component } from 'react'
 import FaShield from 'react-icons/lib/fa/shield'
 
 class Member extends Component {
+  componentWillMount() {
+    this.style = {
+      backgroundColor: 'gray'
+    }
+  }
   render() {
     const { name, thumbnail, email, admin, makeAdmin } = this.props
 
     return (
-      <div className="member">
+      <div className="member" style={this.style}>
         <h1>{name} {(admin) ? <FaShield /> : null}</h1>
         <a onClick={makeAdmin}>Make Admin</a>
         <img src={thumbnail} alt="profile picture" />
